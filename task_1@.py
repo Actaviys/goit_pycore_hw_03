9f24d8cea99d3a3d55e4661b70b6af9b6648eeca
Actaviys <163467524+Actaviys@users.noreply.github.com>
2024-07-15 20:09:53 +0300

    HW

--- task_1@.py
+++ task_1@.py
@@ -1,36 +1,32 @@
  
 from datetime import datetime #Імпортуємо бібліотеку
 
+date_str = input("Введіть дату: ") #Записую в змінну
+
+
 def get_days_from_today(date_str): #Функція обрахунку
-    
     date_s = 0 #Сюди зберігаю вхідний список 
-
+    
     date_s = date_str.split("-")#Робимо список вхідної дати і обрізаємо "-"
-   
+
     #Розбиваємо список по змінних
-    year_in = int(date_s[0])
-    month_in = int(date_s[1])
-    day_in = int(date_s[2])
-            
-        
-    date_parameters = datetime(year=year_in, month=month_in, day=day_in).date()  #Створюємо змінну з параметрами вхідного списку
+    # year_in = date_s[0]
+    # month_in = date_s[1]
+    # day_in = date_s[2]
+    try:
+        date_parameters = datetime(year=int(date_s[0]), month=int(date_s[1]), day=int(date_s[2])).date()  #Створюємо змінну з параметрами вхідного списку
+        now_date = datetime.today().date() #Створюємо змінну з поточною датою 
+        date_r = now_date - date_parameters    # Шукаємо різницю
+        print(date_r)
+    except:
+        print("Невірний формат дати")
+        print("Введіть в форматі: (yyyy-mm-dd)-2021-12-09")
 
-    now_date = datetime.today().date() #Створюємо змінну з поточною датою 
-    date_str = now_date - date_parameters    # Шукаємо різницю
     
     
-    return date_str  #Повертаємо значення
-
-
-rs = input("Введіть дату: ") #Записую в змінну
-
-#Обробляю якщо ввели не правильну дату
-try:
-    get_days_from_today(rs)
-except:
-    print("Невірний формат дати, ведіть в форматі(2021-12-09): ")
-    rs = input("Введіть дату: ") #Записую в змінну
-
-res = get_days_from_today(rs)
     
-print(res)   #Виводжу результат
+
+
+# 2021-12-09
+
+get_days_from_today(date_str)

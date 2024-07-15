479a60cc2fdf243d032fe89af8e8bfc163ffe320
Actaviys <163467524+Actaviys@users.noreply.github.com>
2024-07-15 20:10:59 +0300

    HW

--- task_1@.py
+++ task_1@.py
@@ -8,11 +8,7 @@ def get_days_from_today(date_str): #Функція обрахунку
     date_s = 0 #Сюди зберігаю вхідний список 
     
     date_s = date_str.split("-")#Робимо список вхідної дати і обрізаємо "-"
-
-    #Розбиваємо список по змінних
-    # year_in = date_s[0]
-    # month_in = date_s[1]
-    # day_in = date_s[2]
+    
     try:
         date_parameters = datetime(year=int(date_s[0]), month=int(date_s[1]), day=int(date_s[2])).date()  #Створюємо змінну з параметрами вхідного списку
         now_date = datetime.today().date() #Створюємо змінну з поточною датою 
@@ -21,11 +17,6 @@ def get_days_from_today(date_str): #Функція обрахунку
     except:
         print("Невірний формат дати")
         print("Введіть в форматі: (yyyy-mm-dd)-2021-12-09")
-
-    
-    
-    
-
 
 # 2021-12-09
 
